df1c7a6ef075ce6999e1b8c2fcfc38467551ccd2
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-02-29 21:35:40 -0300

    feat: add participants and remove

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import { styles } from "./styles";
+import {StatusBar} from 'expo-status-bar';
+import {FlatList, ScrollView, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {styles} from "./styles";
 import {useState} from "react";
+import {Participant} from "../../Components/Participant";
+
+interface ParticipantData {
+    id: string,
+    name: string
+}
 
 export const Home = () => {
 
-    const [participant, setParticipant] = useState('');
-    const [participants, setParticipants] = useState([]);
+    const [newParticipant, setNewParticipant] = useState('');
+    const [participants, setParticipants] = useState<ParticipantData[]>([]);
 
     const handleParticipantAdd = () => {
+        const data: ParticipantData = {
+            id: String(new Date().getTime()),
+            name: newParticipant
+        }
 
+        if (newParticipant === '' || participants.some(participant => participant.name === newParticipant)) return;
+
+        setParticipants(oldState => [...oldState, data]);
+    }
+
+    const handleParticipantRemove = (id: string) => {
+        setParticipants(oldState => oldState.filter(
+                participant => participant.id !== id
+            )
+        )
     }
 
     return (
         <View style={styles.container}>
-            <StatusBar style="dark" />
+            <StatusBar style="dark"/>
 
             <Text style={styles.eventName}>Nome do evento</Text>
             <Text style={styles.eventDate}>Quinta, 29 de Fevereiro de 2024</Text>
@@ -24,7 +44,7 @@ export const Home = () => {
                     style={styles.input}
                     placeholder="Nome do participante"
                     placeholderTextColor="#6B6B6B"
-                    onChangeText={setParticipant}
+                    onChangeText={setNewParticipant}
                 />
                 <TouchableOpacity
                     style={styles.button}
@@ -34,6 +54,21 @@ export const Home = () => {
                         +
                     </Text>
                 </TouchableOpacity>
+            </View>
+
+            <View style={styles.participantsContainer}>
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    nestedScrollEnabled={true}
+                    data={participants}
+                    renderItem={({item}) => (
+                        <Participant
+                            name={item.name}
+                            onRemove={() => handleParticipantRemove(item.id)}
+                        />
+                    )}
+                    keyExtractor={item => item.id}
+                />
             </View>
 
         </View>
--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -13,6 +13,9 @@ export const styles = StyleSheet.create({
         marginTop: 36,
         marginBottom: 42,
     },
+    participantsContainer: {
+        flex: 1,
+    },
     eventName: {
         color: '#FFF',
         fontSize: 24,
